4aac29896834695195283b537f05a075ff7eecb9
tanhanze <tanhanze@qq.com>
2023-03-24 21:59:29 +0800

    fuck it

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hiyobot', # 你的项目名称
-    version='0.0.4', # 你的项目版本
+    version='0.0.5', # 你的项目版本
     description='A simple bot framework for Hack.chat.', # 你的项目简介
     long_description=open('README.md').read(), # 你的项目详细介绍，一般从README.md文件中读取
     long_description_content_type='text/markdown', # 你的项目详细介绍的格式，一般为markdown格式
@@ -14,7 +14,6 @@ setup(
         'Development Status :: 3 - Alpha', # 开发状态，一般为Alpha（初级）、Beta（中级）或者Stable（稳定）
         'Intended Audience :: Developers', # 目标受众，一般为Developers（开发者）、End Users/Desktop（桌面端用户）等
         'Topic :: Software Development :: Libraries :: Python Modules', # 主题，一般为Software Development（软件开发）、Scientific/Engineering（科学/工程）等
-        'License :: OSI Approved :: GPL-2.0 License', # 许可证，需要与license参数保持一致
         'Programming Language :: Python :: 3', # 编程语言，一般为Python :: 3或者Python :: 2
         'Programming Language :: Python :: 3.10', # 编程语言的具体版本，根据你的项目支持的python版本选择
     ],
