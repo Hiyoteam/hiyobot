63a9e3827fdcfd4db18eea937366290f8a88dc7f
tanhanze <tanhanze@qq.com>
2023-04-01 21:33:30 +0800

    a

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'Programming Language :: Python :: 3.10', # 编程语言的具体版本，根据你的项目支持的python版本选择
     ],
     keywords='bot hack.chat hack.chat-bot', # 一些关键词，用于描述你的项目特点或者功能
-    packages=["__init__.py"], # 需要打包的python模块，一般使用find_packages函数自动查找，排除测试模块等不需要打包的模块
+    packages=["__init__"], # 需要打包的python模块，一般使用find_packages函数自动查找，排除测试模块等不需要打包的模块
     install_requires=[ # 需要安装的依赖包，一般从requirements.txt文件中读取或者手动列出
         'websocket-client',
         'uuid',
