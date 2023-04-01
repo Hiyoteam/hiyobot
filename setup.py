507abc456d18e6b79136a648c7cb370bc54c12f1
tanhanze <tanhanze@qq.com>
2023-04-01 21:30:29 +0800

    awa

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hiyobot', # 你的项目名称
-    version='0.1.0', # 你的项目版本
+    version='0.1.1', # 你的项目版本
     description='A simple bot framework for Hack.chat.', # 你的项目简介
     long_description=open('README.md').read(), # 你的项目详细介绍，一般从README.md文件中读取
     long_description_content_type='text/markdown', # 你的项目详细介绍的格式，一般为markdown格式
@@ -18,7 +18,7 @@ setup(
         'Programming Language :: Python :: 3.10', # 编程语言的具体版本，根据你的项目支持的python版本选择
     ],
     keywords='bot hack.chat hack.chat-bot', # 一些关键词，用于描述你的项目特点或者功能
-    packages=find_packages(), # 需要打包的python模块，一般使用find_packages函数自动查找，排除测试模块等不需要打包的模块
+    packages=["__init__.py"], # 需要打包的python模块，一般使用find_packages函数自动查找，排除测试模块等不需要打包的模块
     install_requires=[ # 需要安装的依赖包，一般从requirements.txt文件中读取或者手动列出
         'websocket-client',
         'uuid',
